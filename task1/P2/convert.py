1669e99d9f0676961ab5cdcfc7ef07261a42f839
danish-angural <danishangural28@gmail.com>
2020-09-13 13:31:32 +0530

    improved task1

--- task1/P2/convert.py
+++ task1/P2/convert.py
@@ -1,16 +1,14 @@
 import numpy as np
 np.set_printoptions(suppress=True)
-data=np.genfromtxt('../P1/mumbai_data.csv',dtype=float, delimiter=',', skip_header=1)
+data=np.genfromtxt('./mumbai_data.csv',dtype=float, delimiter=',', skip_header=1)
 data=data.transpose()
-_2=np.round(data[2]/data[1], decimals=3)
-_1=np.round(data[1]/20.4)
-data[2]=_2
-data[1]=_1
+data[2]=np.round(data[2]/data[1], decimals=3)
+data[1]=np.round(data[1]/20.4)
 data=data.transpose()
 data=np.delete(data, 0, 1)
 data=data.astype(np.str)
 data=np.char.rstrip(data, chars='.0')
-fields=np.array(['Day','Tests per million', 'Test positivity rate', 'Recovered', 'Deceased' ])
+fields=np.array(['Day','Tests per million', 'Test Positivity rate', 'Recovered', 'Deceased' ])
 days=np.genfromtxt('./mumbai_data.csv',dtype=str, delimiter=',', skip_header=1).transpose()[0]
 data=np.insert(data, 0, days, axis=1)
 data=np.insert(data, 0, fields, 0)
--- task1/P3/merge.py
+++ task1/P3/merge.py
@@ -2,16 +2,11 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 unlock=np.genfromtxt('./mumbai_unlock.csv', dtype=float, delimiter=",", skip_header=1)
-lock=np.genfromtxt('../P1/mumbai_data.csv', dtype=float, delimiter=",", skip_header=1)
-infected_lock=[]
-rate_lock=[]
-infected_unlock=[]
-rate_unlock=[]
-for i in range(len(lock)):
-    infected_lock.append( lock[i][2])
-    rate_lock.append(np.round(lock[i][2]/lock[i][1], decimals=3))
-    infected_unlock.append(unlock[i][2])
-    rate_unlock.append(np.round(unlock[i][2]/unlock[i][1], decimals=3))
+lock=np.genfromtxt('./mumbai_data.csv', dtype=float, delimiter=",", skip_header=1)
+infected_lock= lock[:, 2]
+rate_lock=np.round(lock[:,2]/lock[:,1], decimals=3)
+infected_unlock=unlock[:,2]
+rate_unlock=np.round(unlock[:,2]/unlock[:,1], decimals=3)
 data=np.array([infected_unlock, infected_lock, rate_lock, rate_unlock]).transpose().astype(np.str)
 fields=np.array(['Day','Infected(Unlock)','Infected(Lock)','Positivity Rate(Lock)','Positivity Rate(UnLock)'])
 days=np.genfromtxt('./mumbai_data.csv',dtype=str, delimiter=',', skip_header=1).transpose()[0]
